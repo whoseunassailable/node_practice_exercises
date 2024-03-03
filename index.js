9d800621f75ff5cacb3256a24dc5bc96bed5831f
whoseunassailable <69188520+whoseunassailable@users.noreply.github.com>
2024-03-03 21:39:02 +0530

    Add dynamic routes through Express

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ var app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Amateur developer : Rohan Bhande');
+var fs = require('fs');
+var users = [];
+
+fs.readFile('users.json', { encoding: 'utf-8' }, (err, data) => {
+    if (err) throw err;
+
+    const jsonData = JSON.parse(data);
+    jsonData.people.forEach(user => {
+        users.push(user);
+    });
 });
 
-app.get('/yo', (req, res) => {
-    res.send('hola hijos de puta');
+app.get('/', (req, res) => {
+    var buffer = '';
+
+    users.forEach((user) => {
+        buffer += '<a href="/' + user.first_name + '">' + user.last_name + '</a><br>';
+    });
+
+    res.send(buffer);
 });
+
+app.get('/:first_name', (req, res) => {
+    res.send('The username is ' + req.params.first_name);
+})
 
 var server = app.listen(port, () => {
     console.log('Server running at : ' + server.address().port);
