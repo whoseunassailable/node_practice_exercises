54af212195d915491c415094885582c57f00c24a
whoseunassailable <69188520+whoseunassailable@users.noreply.github.com>
2024-03-19 16:24:01 +0530

    Filtering Paths and Serving HTML Pages - II

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 // admin /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/admin/add-product" method="POST"><input type ="text" name="title"><button type="send">Send</button></form>'
-    );
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST 
